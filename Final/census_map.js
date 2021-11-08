e1e6b1543c7acdf140e49443b6b6b9de34efc58b
wycislo <wycislo@yahoo.com>
2021-11-07 21:28:00 -0600

    final

--- Final/census_map.js
+++ Final/census_map.js
@@ -11,7 +11,7 @@ var topo = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
     'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
 });
 
-var geoJsonLocation = "48.geojson";
+var geoJsonLocation = "harris.geojson";
 var csvLocation = "SVI2018_US_small.csv";
 
 d3.csv(csvLocation).then(function (data) {
